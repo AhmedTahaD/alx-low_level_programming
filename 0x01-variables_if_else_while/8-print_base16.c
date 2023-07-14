47ad218fc46f1a26fadf255930e736490f896880
AhmedTahaD <ahmedtaha2893@gmail.com>
2023-07-14 10:00:09 -0700

    print the numbers in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+/**
+ * print all the numbers of base 16 
+ * author : Ahmed Taha
+ */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char letter;
+
+	for (n = 0; n < 10; n++)
+		putchar('0' + n);
+
+	for (letter = 'a'; letter <= f; letter++)
+		putchar(letter);
+	
+	putchar('\n');
+	return (0);
+}

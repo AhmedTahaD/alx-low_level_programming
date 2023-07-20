0552a86c7abb1e5f1ecb1defcfbd9309cf4823d2
AhmedTahaD <ahmedtaha2893@gmail.com>
2023-07-20 09:02:14 -0700

    print numbers from  0>9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  *print_numbers - function print numbers from 0 > 9
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _putchar - function print char
+ * @char: input
+ * Return: char
+ */
+
+int _putchar(char c)
+{
+	putchar(c);
+	return (0);
+}

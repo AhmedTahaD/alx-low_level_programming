e135551b67de6569472fcc9c3fdc3ec7320da9fb
AhmedTahaD <ahmedtaha2893@gmail.com>
2023-07-20 09:12:32 -0700

    print numbers from 0to9 except2 &4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - function prints the numbers from 0 to 9
+ * exepect 2 & 4
+ * Return: returns 0
+ */
+void print_most_numbers(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		if (number == 2 || number == 4)
+		{
+			continue;
+		}
+		_putchar('0' + number);
+	}
+	_putchar('\n');
+	return (0);
+}

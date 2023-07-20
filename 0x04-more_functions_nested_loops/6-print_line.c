55c2812baa99cb35a6ec241c9ac2149565a24c8e
AhmedTahaD <ahmedtaha2893@gmail.com>
2023-07-20 10:51:43 -0700

    print - n times

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,9 @@
 
 void print_line(int n)
 {
-	int print_num;
-
-	if (n > 0)
+	while (n-- > 0)
 	{
-		for (print_num = 0; print_num < n; print_num++)
-		{
-			_putchar('-');
-		}
+		_putchar('_');
 	}
 	_putchar('\n');
 }

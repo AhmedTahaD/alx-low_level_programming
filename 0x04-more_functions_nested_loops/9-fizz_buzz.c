eb22f7712f171af690237419e263254f5192a351
AhmedTahaD <ahmedtaha2893@gmail.com>
2023-07-20 14:13:08 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ *main - fizz buzz
+ *Return: always 0
+ */
+
+int main(void)
+{
+	int number;
+
+	number = 0;
+	while (number++ < 100)
+	{
+		if ((number % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((number % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else if (((number % 3) == 0) && ((number % 5) == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", number);
+		}
+		if (number != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}

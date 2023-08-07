5f3c0c987fd937f0f645adf860ba1cbceab4e0b5
AhmedTahaD <ahmedtaha2893@gmail.com>
2023-08-07 12:35:08 -0700

    prints a grid of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,9 +8,9 @@
  */
 int **alloc_grid(int width, int height)
 {
-        int **grid;
+	int **grid;
 	int x, y;
-	
+
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	grid = malloc(sizeof(int *) * height);

2f68727426504cad38c8a97015d594e4f0347b1e
AhmedTahaD <ahmedtaha2893@gmail.com>
2023-08-16 10:32:45 -0700

    calculator

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+/**
+ * op_add - sum.
+ * @a: The first number.
+ * @b: The second number.
+ * Return: The sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substraction.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplication.
+ * @a: The first number.
+ * @b: The second number.
+ * Return: The product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division.
+ * @a: The first number.
+ * @b: The second number.
+ * Return: The quotient of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - remainder of the division.
+ * @a: The first number.
+ * @b: The second number.
+ * Return: The remainder of the division of a by b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

b6373481d8bfa663c8d4a2d3aa0490cdf56c5e42
AhmedTahaD <ahmedtaha2893@gmail.com>
2023-08-24 11:46:24 -0700

    add new node at the beginning

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,7 +18,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (!new_node)
 		return (NULL);
 	new_node->str = strdup(str);
-	new_node->count = count;
+	new_node->len = count;
 	new_node->next = (*head);
 	(*head) = new_node;
 	return (*head);

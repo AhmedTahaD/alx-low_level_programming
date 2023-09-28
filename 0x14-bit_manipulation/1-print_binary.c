47fe2869557b06bbc4ed2619bfc7e50112fbf331
AhmedTahaD <ahmedtaha2893@gmail.com>
2023-09-28 22:19:38 +0300

    decimal to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+# include "main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to be printed in binary form
+ */
+void print_binary(unsigned long int n)
+{
+	int temp_num = n;
+	int i;
+	char binary_digit[32];
+	int j;
+
+	if (temp_num == 0)
+		_putchar('0');
+	for (i = 0; temp_num > 0; i++)
+	{
+		binary_digit[i] = temp_num % 2;
+		temp_num = temp_num / 2;
+	}
+	for (j = (i - 1); j >= 0; j--)
+		_putchar(binary_digit[j] + '0');
+	_putchar('\n');
+}

595f84eb69ae0a9d0cf5ad6733cc3b0568239b65
AhmedTahaD <ahmedtaha2893@gmail.com>
2024-01-11 21:50:08 +0200

    inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,43 +8,31 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node;
-	dlistint_t *head;
-	unsigned int i;
+	dlistint_t *new_node, *res_head = *h;
 
-	new_node = NULL;
 	if (idx == 0)
-		new_node = add_dnodeint(h, n);
-	else
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
 	{
-		head = *h;
-		i = 1;
-		if (head != NULL)
-			while (head->prev != NULL)
-				head = head->prev;
-		while (head != NULL)
-		{
-			if (i == idx)
-			{
-				if (head->next == NULL)
-					new_node = add_dnodeint_end(h, n);
-				else
-				{
-					new_node = malloc(sizeof(dlistint_t));
-					if (new_node != NULL)
-					{
-						new_node->n = n;
-						new_node->next = head->next;
-						new_node->prev = head;
-						head->next->prev = new_node;
-						head->next = new_node;
-					}
-				}
-				break;
-			}
-			head = head->next;
-			i++;
-		}
+		if (res_head == NULL)
+			return (NULL);
+		res_head = res_head->next;
 	}
+
+	if (res_head->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = res_head->next;
+	new_node->prev = res_node;
+	res_head->next->prev = new_node;
+	res_head->next = new_node;
+
 	return (new_node);
 }
